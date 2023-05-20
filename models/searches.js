f7e233281c8990fd0d5cec95fc8eb77d6fdfb012
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-05-19 23:43:48 -0500

    Mapbox search API and token

--- models/searches.js
+++ models/searches.js
@@ -9,7 +9,16 @@ class Searches {
 
   async city(place = "") {
     //http
-    console.log("city", place);
+    // console.log("city", place);
+
+    try {
+      const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/bogota.json?access_token=pk.eyJ1IjoiY2FtaWxvY29ydGVzMTc1MyIsImEiOiJjbGh2aHVwYW8wOGZxM2RxZmRkdmp4eDRiIn0.CYMUQBePcfXrAnlaf0iXew&limit=5&language=es");
+      console.log(resp.data);
+
+      return [];
+    } catch (error) {
+      return [];
+    }
 
     return [];
   }
